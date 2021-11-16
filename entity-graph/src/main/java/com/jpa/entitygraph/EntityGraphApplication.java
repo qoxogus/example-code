d2893e4d86b6508f99fa25def85ae0f92f30528b
qoxogus <a01034367055@gmail.com>
2021-11-16 14:20:08 +0900

    [ADD] n + 1 프로젝트 생성

---
+++ entity-graph/src/main/java/com/jpa/entitygraph/EntityGraphApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.entitygraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityGraphApplication.class, args);
+	}
+
+}
